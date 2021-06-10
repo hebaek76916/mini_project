ba1e237ebebfdf1ca8c63a2b02c728ffb92145e3
hebaek76916 <qorgusdms15@gmail.com>
2021-06-11 01:06:32 +0900

    done

--- COVIDCHART_IOSACADEMY/COVIDCHART_IOSACADEMY/APICaller.swift
+++ COVIDCHART_IOSACADEMY/COVIDCHART_IOSACADEMY/APICaller.swift
@@ -6,7 +6,23 @@
 //
 
 import Foundation
-//https://api.covidtracking.com/v2/states.json
+extension DateFormatter {
+    static let dayFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY-MM-dd"
+        formatter.timeZone = .current
+        formatter.locale = .current
+        return formatter
+    }()
+    static let prettyFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeZone = .current
+        formatter.locale = .current
+        return formatter
+    }()
+}
+
 class  APICaller {
     static let shared = APICaller()
     
@@ -22,7 +38,7 @@ class  APICaller {
     
     public func getCovidData (
         for scope: DataScope,
-        completion: @escaping (Result<String, Error>) -> Void
+        completion: @escaping (Result<[DayData], Error>) -> Void
     ) {
         let urlString: String
         switch scope {
@@ -39,8 +55,14 @@ class  APICaller {
         let task = URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data, error == nil else { return }
             do {
-                let result = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-                print(result)
+                let result = try JSONDecoder().decode(CovidDataResponse.self, from: data)
+                let models: [DayData] = result.data.compactMap {
+                    guard let value = $0.cases.total.value,
+                        let date = DateFormatter.dayFormatter.date(from: $0.date) else { return nil }
+                    return DayData(date: date,
+                            count: value)
+                }
+                completion(.success(models))
             } catch {
                 completion(.failure(error))
             }
@@ -77,4 +99,24 @@ struct StateListResponse: Codable {
 struct State: Codable {
     let name: String
     let state_code: String
+}
+
+struct CovidDataResponse: Codable {
+    let data: [CovidDayData]
+}
+struct CovidDayData: Codable {
+    let cases: CovidCases
+    let date: String
+}
+
+struct CovidCases: Codable {
+    let total: TotalCases
+}
+struct TotalCases: Codable {
+    let value: Int?
+}
+
+struct DayData {
+    let date: Date
+    let count: Int
 }
--- COVIDCHART_IOSACADEMY/COVIDCHART_IOSACADEMY/ViewController.swift
+++ COVIDCHART_IOSACADEMY/COVIDCHART_IOSACADEMY/ViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import Charts
 
 
 // Data of COVID Cases
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+                      UITableViewDataSource {
 
     /*
      - Call APIs
@@ -18,21 +20,80 @@ class ViewController: UIViewController {
      - Filter / User Pick the State
      - Update UI
      */
+    static let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.groupingSeparator = ","
+        formatter.formatterBehavior = .default
+        formatter.locale = .current
+        
+        return formatter
+    }()
+    
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var dayData: [DayData] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.createGraph()
+            }
+            
+        }
+    }
     
     private var scope: APICaller.DataScope = .national
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "COVID Cases"
+        configureTable()
         createFilterButton()
         fetchData()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func createGraph() {
+        let headerView = UIView(
+            frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.width / 1.5))
+        headerView.clipsToBounds = true
+        
+        let set = dayData.prefix(20)
+        var entries: [BarChartDataEntry] = []
+        for index in 0..<set.count {
+            let data = set[index]//dayData[index]
+            entries.append(.init(x: Double(index), y: Double(data.count)))
+        }
+        let dataSet = BarChartDataSet(entries: entries)
+        dataSet.colors = ChartColorTemplates.material()
+        
+        let data: BarChartData = BarChartData(dataSet: dataSet)
+        
+        let chart = BarChartView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width,
+                                               height: view.frame.size.width/1.5))
+        chart.data = data
+        headerView.addSubview(chart)
+        
+        tableView.tableHeaderView = headerView
+    }
+    private func configureTable() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+    }
     private func fetchData() {
-        APICaller.shared.getCovidData(for: scope) { result in
+        APICaller.shared.getCovidData(for: scope) { [weak self] result in
             switch result {
-            case .success(let data):
-                break
+            case .success(let dayData):
+                print(dayData.count)
+                self?.dayData = dayData
             case .failure(let error):
                 print(error)
             }
@@ -58,7 +119,20 @@ class ViewController: UIViewController {
         let navVC = UINavigationController(rootViewController: vc)
         present(navVC, animated: true)
     }
-
-
+    //MARK - Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dayData.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = dayData[indexPath.row]
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = createText(with: data)
+        return cell
+    }
+    private func createText(with data: DayData) -> String? {
+        let dateString = DateFormatter.prettyFormatter.string(from: data.date)
+        let total = Self.numberFormatter.string(from: NSNumber(value: data.count))
+        return "\(dateString): \(total ?? "\(data.count)")"
+    }
 }
 
